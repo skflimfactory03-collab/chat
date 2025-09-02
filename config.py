ef3fd71869b3d97fbd6ed7bfc9cc5d069a820f97
skflimfactory03-collab <skflimfactory03@gmail.com>
2025-09-02 11:42:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-BOT_TOKEN = "8339967007:AAFk6k-gsD1y7L8y_1Fj1CLGTGuGiAwmC9I"
+BOT_TOKEN = "8414250427:AAH-OeeidFYWDJCexT0hvTdVHl_Ww5T8EAY"
 SUPPORT_LINK = "https://t.me/chuckymusic_support"
 UPDATES_LINK = "https://t.me/teakadai_update"
 START_PHOTO_URL = "https://cf-media.geiccobs.com/banner.jpg"  # Apne hosted ya imgur photo ka link yahan dalo
